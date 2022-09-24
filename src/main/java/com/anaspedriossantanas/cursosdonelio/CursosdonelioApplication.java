21f1b593d156de65a014f5b324041a57ab126664
AnaSantana <anapedra.mil@gmail.com>
2022-09-24 17:05:24 -0300

    promeiro cimit

---
+++ src/main/java/com/anaspedriossantanas/cursosdonelio/CursosdonelioApplication.java
@@ -0,0 +1,13 @@
+package com.anaspedriossantanas.cursosdonelio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosdonelioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursosdonelioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anaspedriossantanas/cursosdonelio/CursosdonelioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anaspedriossantanas.cursosdonelio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosdonelioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
